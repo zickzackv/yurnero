7b6e6ecde2eaa9e7014e8867ee0b29cf20d27704
Fabian Otto <fabian.otto@gmail.com>
2026-05-24 20:29:28 +0200

    chore: using dev/by-id/ for disko

--- hosts/yurnero/disko.nix
+++ hosts/yurnero/disko.nix
@@ -2,13 +2,15 @@
 {
   disko.devices = {
     disk = {
-      sda = {
+      system = {
         type = "disk";
-        device = "/dev/sda";
+        device = "/dev/disk/by-id/ata-KINGSTON_SUV400S37480G_50026B7766031305";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
+              priority = 1;
+              name = "ESP";
               type = "EF00";
               size = "512M";
               content = {
@@ -20,6 +22,8 @@
             };
 
             root = {
+              priority = 2;
+              name = "nixos-root";
               size = "100%";
               content = {
                 type = "filesystem";
