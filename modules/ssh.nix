77f126b37e95dee49515315a73efe8c77b788d87
Fabian Otto <fabian.otto@gmail.com>
2026-05-24 21:12:28 +0200

    fix: rename ssh key user admin to fabian

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -11,7 +11,7 @@
 
   networking.firewall.allowedTCPPorts = [ 22 ];
 
-  users.users.admin.openssh.authorizedKeys.keys = [
+  users.users.fabian.openssh.authorizedKeys.keys = [
     # Exportiere deinen Public Key aus 1Password und ersetze diesen Platzhalter.
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnhG9cVWWKezu+IUcLkNbhsKFA87F226zTXgS7QlCMJ fabian@traxex"
   ];
