5f2c792505bb2560c9050929fc8229c2db26ee5d
Fabian Otto <fabian.otto@gmail.com>
2026-05-24 19:53:01 +0200

    fix(ssh): changing pub key

--- modules/ssh.nix
+++ modules/ssh.nix
@@ -13,6 +13,6 @@
 
   users.users.admin.openssh.authorizedKeys.keys = [
     # Exportiere deinen Public Key aus 1Password und ersetze diesen Platzhalter.
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAI_REPLACE_THIS_WITH_YOUR_REAL_PUBLIC_KEY admin@server"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnhG9cVWWKezu+IUcLkNbhsKFA87F226zTXgS7QlCMJ fabian@traxex"
   ];
 }
