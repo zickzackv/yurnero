5671b5c22a067157bc4dc7e8c658ff3555e712c7
Fabian Otto <fabian.otto@gmail.com>
2026-06-12 19:22:03 +0200

    feat(flake): adding a wakonlan app for waking yurnero

--- flake.nix
+++ flake.nix
@@ -7,17 +7,34 @@
     disko.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, disko, ... }:
-    let
-      system = "x86_64-linux";
-    in {
-      nixosConfigurations.yurnero = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit self; };
-        modules = [
-          disko.nixosModules.disko
-          ./hosts/yurnero/configuration.nix
-        ];
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    disko,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system;};
+    yurneroAddr = "D0:17:C2:93:B3:B5";
+  in {
+    apps.${system}.wakonlan = {
+      type = "app";
+      program = "${(pkgs.writeShellApplication {
+        name = "wakonlan";
+        runtimeInputs = [pkgs.wakeonlan];
+        text = ''
+          exec wakeonlan ${yurneroAddr}
+        '';
+      })}/bin/wakonlan";
     };
+
+    nixosConfigurations.yurnero = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit self;};
+      modules = [
+        disko.nixosModules.disko
+        ./hosts/yurnero/configuration.nix
+      ];
+    };
+  };
 }
